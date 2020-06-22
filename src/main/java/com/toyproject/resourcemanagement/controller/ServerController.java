adeb76f4a36f467bc30a6bf65e1b0b23a44b40c5
Abufarhah <laythabufarhah@gmail.com>
2020-06-22 17:09:51 +0300

    some services were added

--- src/main/java/com/toyproject/resourcemanagement/controller/ServerController.java
+++ src/main/java/com/toyproject/resourcemanagement/controller/ServerController.java
@@ -2,6 +2,7 @@ package com.toyproject.resourcemanagement.controller;
 
 import com.toyproject.resourcemanagement.model.Server;
 import com.toyproject.resourcemanagement.service.ServerService;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,51 @@ import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/CloudService",method=RequestMethod.GET)
 public class ServerController {
-    private static final Logger log = LoggerFactory.getLogger(ServerService.class);
+    private static final Logger log = LoggerFactory.getLogger(ServerController.class);
 
     @Autowired
     ServerService serverService;
 
-    @RequestMapping(path = "/servers", method = RequestMethod.GET)
-    public ResponseEntity<String> getServer(@RequestParam int size) {
-        log.info("controller");
+    @RequestMapping(path = "/api",method=RequestMethod.GET)
+    public ResponseEntity<String> api(){
+        return new ResponseEntity<String>("Welcome to the cloud service api",HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/servers")
+    public ResponseEntity<List<Server>> getServers() {
+        log.info("getServers");
+        try {
+            List<Server> serverList = serverService.getServers();
+            return new ResponseEntity<List<Server>>(serverList,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<List<Server>>(new ArrayList<Server>(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/servers/{id}")
+    public ResponseEntity<Server> getServer(@PathVariable int id) {
+        log.info("getServer");
+        try {
+            Server server= serverService.getServer(id);
+            if(server!=null) {
+                return new ResponseEntity<Server>(server, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<Server>(new Server(),HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<Server>(new Server(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/servers/allocate")
+    public ResponseEntity<String> allocateServer(@NotEmpty @RequestParam int size) {
+        log.info("allocateServer");
         String responeBody=serverService.allocateServer(size);
         return new ResponseEntity<String>(responeBody,HttpStatus.OK);
     }
--- src/main/java/com/toyproject/resourcemanagement/service/ServerService.java
+++ src/main/java/com/toyproject/resourcemanagement/service/ServerService.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class ServerService {
     private static final Logger log = LoggerFactory.getLogger(ServerService.class);
@@ -23,5 +27,17 @@ public class ServerService {
         if(size<=0) return "Minimum server size is 1 Giga";
         cloudServerProvider.allocateServer(size);
         return "A server with size: "+size+" was allocated";
+    }
+
+    public List<Server> getServers(){
+        List<Server> serverList=new ArrayList<>();
+        serverRepository.findAll().forEach(serverList::add);
+        Collections.sort(serverList);
+        return serverList;
+    }
+
+    public Server getServer(int id){
+        Server server=serverRepository.findOne(id);
+        return server;
     }
 }
